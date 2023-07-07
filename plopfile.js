d4b5ebfb89f253164c2c4572472814f8eff8946a
chenhaojie9527 <a18520145493@163.com>
2023-07-07 23:13:04 +0800

    feat: 生成文件夹目录文本

--- plopfile.js
+++ plopfile.js
@@ -27,4 +27,63 @@ export default function (plop) {
       },
     ],
   });
+  // 生成文件夹目录
+  plop.setGenerator("Folders", {
+    description: "生成节点模板",
+    prompts: [
+      {
+        type: "input",
+        name: "nodeName",
+        message: "请输入文件夹名称[eg:fls-contract]",
+        default: "fls-demo",
+        validate: function (val) {
+          if (/.+/.test(val)) {
+            if (val.length < 6 || val.length > 20) {
+              return "文件夹名称长度[6-20]的小写英文单词";
+            }
+            return true;
+          } else {
+            return "文件夹名称不能为空!";
+          }
+        },
+      },
+      {
+        type: "input", // 交互类型,字符输入
+        name: "funCode", //参数名称
+        message: "请输入节点编码[eg:fls0001]", // 交互提示
+        default: "fls0000",
+        validate: function (val) {
+          if (!!val && val.length == 7) {
+            return true;
+          } else {
+            return "请保持节点编码长度为7";
+          }
+        },
+      },
+      {
+        type: "list", // 交互类型: 上下键选择
+        name: "server", //参数名称
+        message: "请选择后端服务:", // 交互提示
+        choices: [
+          "imfbp-fls-before-web",
+          "imfbp-fls-after-web",
+          "imfbp-crm-web",
+          "imfbp-invoice-web",
+        ],
+        default: "imfbp-fls-before-web",
+      },
+    ],
+    actions: (data) => {
+      const nodeName = "{{ nodeName }}";
+      return [
+        {
+          type: "addMany", // 增加多个文件
+          base: "template/layout/", // 生成,目标路径会去掉这写路径
+          stripExtensions: true,
+          destination: `ucf-apps/${nodeName}/`, // 目标路径
+          templateFiles: "template/layout/src/**/**", // 配置的模板文件路径.
+        },
+      ];
+    },
+  });
 }
