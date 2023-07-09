24b05b469e3e8243523835676f91539dfe628c95
chenhaojie9527 <a18520145493@163.com>
2023-07-09 22:27:10 +0800

    feat: 搭建router

--- src/pages/useForm.tsx
+++
@@ -1,7 +0,0 @@
-import React, {FC} from "react"
-
-const useSuForm: FC = () => {
-    return <div>useSuForm</div>
-}
-
-export default useSuForm
---
+++ src/routers/index.ts
@@ -0,0 +1,11 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import Home from "../pages/Home";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    Component: Home,
+  },
+];
+
+export default createBrowserRouter(routes);
