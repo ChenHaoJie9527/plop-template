b7ee5d8dd82f3bc239924092681f80a76f611285
chenhaojie9527 <a18520145493@163.com>
2023-07-12 23:13:38 +0800

    feat: 校验路由组件是否存在

--- template/generator/pageRouter.js
+++ template/generator/pageRouter.js
@@ -1,3 +1,8 @@
+const path = require("path");
+const fs = require("fs");
+const verifyFileExist = require("../utils/verifyFileExist");
+
+const baseFile = path.resolve(__dirname, "../../src/pages");
 module.exports = {
   description: "Generate a new route component",
   prompts: [
@@ -9,28 +14,42 @@ module.exports = {
         if (!dirName || dirName.trim === "") {
           return "路由组件名不能为空";
         }
+        if (verifyFileExist(dirName, baseFile)) {
+          return "路由组件已存在";
+        }
+        return true;
       },
     },
   ],
-  actions: [
-    {
-      type: "add",
-      path: "src/pages/{{pascalCase componentName}}.tsx",
-      templateFile: "template/routerComponents.hbs",
-    },
-    {
-      type: "modify",
-      path: "src/routers/index.ts",
-      pattern: /(\/\/ -- ADD ROUTE COMPONENTS HERE --)/gi,
-      template:
-        "import {{pascalCase componentName}} from '../pages/{{pascalCase componentName}}';\n$1",
-    },
-    {
-      type: "modify",
-      path: "src/routers/index.ts",
-      pattern: /(\/\/ -- ADD ROUTES HERE --)/gi,
-      template:
-        "{ path: '/{{pascalCase componentName}}', Component: {{pascalCase componentName}} },\n$1",
-    },
-  ],
+  actions: (data) => {
+    const basePath = path.join(baseFile, `./${data.componentName}`);
+    console.log("basePath", basePath);
+    // 新建一个组件文件夹
+    fs.mkdirSync(basePath);
+    // 新建一个组件文件夹下的通用文件夹
+    fs.mkdirSync(path.join(baseFile, `./${data.componentName}/components`));
+    return [
+      {
+        type: "add",
+        path: "src/pages/{{pascalCase componentName}}/index.tsx",
+        templateFile: "template/routerComponents.hbs",
+      },
+      {
+        type: "modify",
+        path: "src/routers/index.ts",
+        pattern: /(\/\/ -- ADD ROUTE COMPONENTS HERE --)/gi,
+        force: true,
+        template:
+          "import {{pascalCase componentName}} from '../pages/{{pascalCase componentName}}';\n$1",
+      },
+      {
+        type: "modify",
+        path: "src/routers/index.ts",
+        pattern: /(\/\/ -- ADD ROUTES HERE --)/gi,
+        force: true,
+        template:
+          "{ path: '/{{pascalCase componentName}}', Component: {{pascalCase componentName}} },\n$1",
+      },
+    ];
+  },
 };
--- template/utils/verifyFileExist.js
+++ template/utils/verifyFileExist.js
@@ -3,5 +3,6 @@ const fs = require("fs");
 
 module.exports = (dirName, dirPath, tail = "") => {
   const dir = fs.readdirSync(dirPath);
+  console.log(dir);
   return dir.includes(dirName + tail);
 };
